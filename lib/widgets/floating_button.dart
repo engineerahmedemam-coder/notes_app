622de71128b5e453918a6cfbc2732b966cd94bb9
Ahmed Emam <engineerahmedemam@gmail.com>
2026-04-14 20:09:22 +0200

    add "add note cubit" to "floating button

--- lib/widgets/floating_button.dart
+++ lib/widgets/floating_button.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/add_notes_cubit/cubit/add_notes_cubit.dart';
 import 'package:notes/views/home_view.dart';
 import 'package:notes/widgets/show_modal_container.dart';
-
 class FloatingButton extends StatelessWidget {
-  const FloatingButton({
-    super.key,
-  });
+  const FloatingButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(onPressed: (){
-      showModalBottomSheet(context: context, builder: (context){
-       return ShowModalContainer();
-      });
-    },
-     child: Icon(Icons.add,),backgroundColor: const Color.fromARGB(255, 121, 181, 209),);
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return BlocConsumer<AddNotesCubit, AddNotesState>(
+              listener: (context, state) {
+                // TODO: implement listener
+              },
+              builder: (context, state) {
+                if (state is AddNotesSuccess){
+                  Navigator.pop(context);
+                }
+                if (state is AddNotesFailure) {
+                  print('failed');
+                }
+
+                if (state is AddNotesLoading){
+                return Stack(
+                  children: [ 
+                    
+                    ShowModalContainer(),
+                    if (state is AddNotesLoading) Container(
+          color: Colors.black.withOpacity(0.4),
+          child: const Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+                      
+                    
+
+                  ]
+                  );
+                }
+              },
+            );
+          },
+        );
+      },
+      child: Icon(Icons.add),
+      backgroundColor: const Color.fromARGB(255, 121, 181, 209),
+    );
   }
 }
